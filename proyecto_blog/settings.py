da68259b43d3697fc17a7ec6571d48603981d07b
vmarialuz <vmarialuzm@gmail.com>
2023-10-17 23:51:28 -0500

    settings

--- proyecto_blog/settings.py
+++ proyecto_blog/settings.py
@@ -14,10 +14,13 @@ from pathlib import Path
 import environ
 import os
 
+env = environ.Env(DEBUG=(bool, False))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
