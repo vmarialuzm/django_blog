044bb67d2e4daf9c31e033c7f0b37a72099c7ff1
vmarialuz <vmarialuzm@gmail.com>
2023-10-21 11:05:14 -0500

    modifique el campo image de mi modelo

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to="imagenes/", null=True)
+    image = models.URLField(null=True, blank=True)
     liked_by = models.ManyToManyField(User, related_name='liked_posts', blank=True)
     disliked_by = models.ManyToManyField(User, related_name='disliked_posts', blank=True)
     
--- proyecto_blog/urls.py
+++ proyecto_blog/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +24,3 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
